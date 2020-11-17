069cf2ae77a80b361bc1ba12250f369845fd5e0c
Role Gate <contact@rolegate.com>
2020-11-17 15:31:54 +0100

    End of section 5

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 
@@ -32,7 +33,7 @@ export default function EventListItem({ event, selectEvent, deleteEvent }) {
       <Segment clearing>
         <div>{event.description} </div>
         <Button
-          onClick={() => selectEvent(event)}
+          as = {Link} to = {`/events/${event.id}`}
           color='teal'
           floated='right'
           content='View'
--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import './app/layout/styles.css';
-import App from './app/layout/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import { BrowserRouter } from "react-router-dom";
 
 //import * as serviceWorker from './serviceWorker';
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 
-function render()
-{
+function render() {
   ReactDOM.render(
-  
-    <App />,rootEl
-  
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+
+    rootEl
+  );
 }
-if(module.hot){
-  module.hot.accept('./app/layout/App',function ()
-  {
+if (module.hot) {
+  module.hot.accept("./app/layout/App", function () {
     setTimeout(render);
-  })
+  });
 }
 
 render();
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
